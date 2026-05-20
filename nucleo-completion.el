567eca0990f4ec537136f50b4ce7d6228cdace63
kn66 <kamimoto527@gmai.com>
2026-05-20 21:55:40 +0900

    Bump module version to 0.1.12

--- nucleo-completion.el
+++ nucleo-completion.el
@@ -4,7 +4,7 @@
 
 ;; Author: Nobu <https://github.com/kn66>
 ;; Assisted-by: OpenAI Codex:gpt-5
-;; Version: 0.1.11
+;; Version: 0.1.12
 ;; Package-Requires: ((emacs "27.1"))
 ;; Keywords: matching, convenience
 ;; URL: https://github.com/kn66/nucleo-completion.el
@@ -190,7 +190,7 @@ Load failures are always saved in
   :type 'string
   :group 'nucleo-completion)
 
-(defconst nucleo-completion-required-module-version "0.1.11"
+(defconst nucleo-completion-required-module-version "0.1.12"
   "Version of the prebuilt Rust module expected by this package.")
 
 (defcustom nucleo-completion-module-release-tag nil
